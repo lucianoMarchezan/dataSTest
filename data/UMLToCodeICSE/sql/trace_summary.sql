961ff6d8e4fab33f8232063ad2602c24dd239d4f
lucianoMarchezan <lucianomarchezan94@gmail.com>
2022-10-11 20:36:29 +0200

    removing wrong avg time from traces

--- data/UMLToCodeICSE/sql/trace_summary.sql
+++ data/UMLToCodeICSE/sql/trace_summary.sql
@@ -1,3 +1,4 @@
-SELECT model_name,systems.java_classes_count + systems.java_methods_count + systems.java_fields_count as size, sum(rowCount) as traced, sum(totalTime) as total_time,sum(averageTime) as avg_time
+SELECT model_name,systems.java_classes_count + systems.java_methods_count + systems.java_fields_count as size, sum(rowCount) as traced, sum(totalTime) as total_time,
+sum(totalTime)/(systems.java_classes_count + systems.java_methods_count + systems.java_fields_count) as avg_time
 FROM traces_results 
 INNER JOIN systems ON systems.name=traces_results.model_name  group by model_name;

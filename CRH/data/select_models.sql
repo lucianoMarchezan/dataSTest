23e1fccb5ff6bc2e7533d7fce41b33f1d060211b
lucianoMarchezan <lucianomarchezan94@gmail.com>
2023-04-12 16:36:32 +0200

    updating data

--- CRH/data/select_models.sql
+++ CRH/data/select_models.sql
@@ -1,1 +1,6 @@
-SELECT idsmodels,name, size,cres,cre_dependencies, Round((cre_dependencies*100)/cres,1) as percent, round(cre_depen_time/cres/1000,2) as avg_time_ms FROM dependency_evaluation.models;
+SET @row_number = 0;   
+
+SELECT  (@row_number:=@row_number + 1) AS Id ,name,size,
+cres,concat(cre_dependencies, concat(concat("\\bluewhitebar{",Round((cre_dependencies)/cres,3)),"}")) as CREs_percent, 
+inconsistencies,concat(incon_dependencies,concat(concat("\\redwhitebar{",Round((incon_dependencies)/inconsistencies,3)),"}")) as incon_percent,
+round(cre_depen_time/cres/1000,2) as avg_time_ms FROM dependency_evaluation.models order by size desc;

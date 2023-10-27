185b6c40e4929918de47f2c50680e1532cf9ea37
lucianoMarchezan <lucianomarchezan94@gmail.com>
2023-10-27 13:00:32 +0200

    adding more data

---
+++ data/Saner2024/select_group_repair_results.sql
@@ -0,0 +1,9 @@
+SELECT models.idsmodels, round(avg(number_of_incon),2) as avg_n_incon
+,round(avg(total_repairs),2) as avg_repairs, 
+round(std(total_repairs),2) as std_repair,
+round(avg(overlapping_repairs),2) as avg_over, round(std(overlapping_repairs),2) as std_over, 
+round(avg(conflicting_repairs),2) as avg_conflicting, round(std(conflicting_repairs),2) as std_conflicting,
+round(avg(shared_locations),2) as avg_shared_locations, round(std(shared_locations),2) as std_shared_locations
+FROM saner_24.group_repair_results,  saner_24.models where model_name = name
+group by model_name
+order by models.idsmodels;
--- data/Saner2024/select_repair_results.sql
+++ data/Saner2024/select_repair_results.sql
@@ -1,8 +1,5 @@
-SELECT cr1, round((avg(cre1_repairs)+avg(cre2_repairs))/2,2) as avg_repair, 
-round((std(cre1_repairs)+std(cre2_repairs))/2,2) as std_repair,
-round(avg(overlapping),2) as avg_over,
-round(std(overlapping),2) as std_over, round(avg(conflicting),2) as average_c,
-round(std(conflicting),2) as std_c
-FROM dependency_evaluation.repair_results 
-group by cr1
-order by cr1;
+SELECT models.idsmodels,sum(repairs) as total,
+round(avg(repairs),2) as average ,
+round(std(repairs),2) as std
+ FROM saner_24.repair_results, saner_24.models where models.name=repair_results.model_name
+ group by model_name order by idsmodels;

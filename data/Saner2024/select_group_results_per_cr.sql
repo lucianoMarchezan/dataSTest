0cb392dc6ef71f3946a48115630f5a6dc0f66cba
lucianoMarchezan <lucianomarchezan94@gmail.com>
2023-10-28 13:42:09 +0200

    update repairs per cr

--- data/Saner2024/select_group_results_per_cr.sql
+++ data/Saner2024/select_group_results_per_cr.sql
@@ -1,5 +1,5 @@
-SELECT repair_results.cre, round(avg(number_of_incon),2) as avg_n_incon
-,round(avg(total_repairs),2) as avg_repairs, 
+SELECT repair_results.cre,
+round(avg(total_repairs),2) as avg_repairs, 
 round(std(total_repairs),2) as std_repair,
 round(avg(overlapping_repairs),2) as avg_over, round(std(overlapping_repairs),2) as std_over, 
 round(avg(conflicting_repairs),2) as avg_conflicting, round(std(conflicting_repairs),2) as std_conflicting,

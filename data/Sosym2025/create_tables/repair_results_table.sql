bf481e733c230cfc9e2e7c69bee6c080d4a0bee5
lucianoMarchezan <lucianomarchezan94@gmail.com>
2025-02-06 10:40:55 +0100

    Update repair_results_table.sql

--- data/Sosym2025/create_tables/repair_results_table.sql
+++ data/Sosym2025/create_tables/repair_results_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE `repair_results` (
   `cre` varchar(100) DEFAULT NULL,
   `cr` varchar(50) DEFAULT NULL, 
 `repair` varchar(50) DEFAULT NULL, 
-  `rank` real DEFAULT NULL,  
+  `priority` real DEFAULT NULL,  
   PRIMARY KEY (`idrepair`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
